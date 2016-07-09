b6f507bc3ac2694d9de74b9380ef9d3c908e20f9
jcalebj <tarajones@taras-mbp.attlocal.net>
2016-07-09 13:49:22 -0700

    SearchPage Added
    Index and Search pages have  been added

---
+++ SearchPage.js
@@ -0,0 +1,46 @@
+'use strict';
+var React = require('react');
+var {
+Component
+} = React;
+
+var ReactNative = require('react-native');
+var {
+StyleSheet,
+Text,
+View,
+TouchableHighlight,
+Image,
+ListView
+} = ReactNative;
+
+var styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  }
+});
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
+        </Text>
+      </View>
+    );
+  }
+}
+
+module.exports = SearchPage;
---
+++ index.ios.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+var SearchPage = require('./SearchPage');
+
+var styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinderApp extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
