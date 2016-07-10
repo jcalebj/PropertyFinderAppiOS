0c3eb6ef37efc4d46950abb6c0beb7f7ed257bcd
jcalebj <tarajones@taras-mbp.attlocal.net>
2016-07-09 21:22:45 -0400

    Property View, Search Results, and geo location added

--- SearchPage.js
+++ SearchPage.js
@@ -12,8 +12,11 @@ TextInput,
 View,
 TouchableHighlight,
 Image,
-ListView
+ListView,
+ActivityIndicatorIOS
 } = ReactNative;
+
+var SearchResults = require('./SearchResults');
 
 var styles = StyleSheet.create({
   description: {
@@ -59,11 +62,96 @@ searchInput: {
   borderColor: '#48BBEC',
   borderRadius: 8,
   color: '#48BBEC'
+},
+image: {
+  width: 217,
+  height: 138
 }
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber
+  };
+  data[key] = value;
+ 
+  var querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+ 
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+};
+
 class SearchPage extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    searchString: 'london',
+    isLoading: false,
+    message: ''
+  };
+}
+
+onLocationPressed() {
+  navigator.geolocation.getCurrentPosition(
+    location => {
+      var search = location.coords.latitude + ',' + location.coords.longitude;
+      this.setState({ searchString: search });
+      var query = urlForQueryAndPage('centre_point', search, 1);
+      this._executeQuery(query);
+    },
+    error => {
+      this.setState({
+        message: 'There was a problem with obtaining your location: ' + error
+      });
+    });
+}
+
+_executeQuery(query) {
+  console.log(query);
+  this.setState({ isLoading: true });
+  fetch(query)
+  .then(response => response.json())
+  .then(json => this._handleResponse(json.response))
+  .catch(error =>
+     this.setState({
+      isLoading: false,
+      message: 'Something bad happened ' + error
+   }));
+}
+
+_handleResponse(response) {
+  this.setState({ isLoading: false , message: '' });
+  if (response.application_response_code.substr(0, 1) === '1') {
+    this.props.navigator.push({
+  title: 'Results',
+  component: SearchResults,
+  passProps: {listings: response.listings}
+});
+  } else {
+    this.setState({ message: 'Location not recognized; please try again.'});
+  }
+}
+ 
+onSearchPressed() {
+  var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+  this._executeQuery(query);
+}
+  onSearchTextChanged(event) {
+  console.log('onSearchTextChanged');
+  this.setState({ searchString: event.nativeEvent.text });
+  console.log(this.state.searchString);
+}
   render() {
+    var spinner = this.state.isLoading ?
+  ( <ActivityIndicatorIOS
+      size='large'/> ) :
+  ( <View/>);
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -74,17 +162,21 @@ class SearchPage extends Component {
         </Text>
         <View style={styles.flowRight}>
   <TextInput
-    style={styles.searchInput}
-    placeholder='Search via name or postcode'/>
+  style={styles.searchInput}
+  value={this.state.searchString}
+  onChange={this.onSearchTextChanged.bind(this)}
+  placeholder='Search via name or postcode'/>
   <TouchableHighlight style={styles.button}
-      underlayColor='#99d9f4'>
+      underlayColor='#99d9f4' onPress={this.onSearchPressed.bind(this)}>
     <Text style={styles.buttonText}>Go</Text>
   </TouchableHighlight>
 </View>
 <TouchableHighlight style={styles.button}
-    underlayColor='#99d9f4'>
+    underlayColor='#99d9f4' onPress={this.onLocationPressed.bind(this)}>
   <Text style={styles.buttonText}>Location</Text>
 </TouchableHighlight>
+<Image source={require('./Resources/house.png')} style={styles.image}/>
+<Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
